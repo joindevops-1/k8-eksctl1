8a6472f40e306261636b73a8fb8f1e47de86abfd
sivakumarreddym <info@joindevops.com>
2025-03-03 19:45:08 +0530

    expense infra

---
+++ docker.tf
@@ -0,0 +1,77 @@
+resource "aws_instance" "this" {
+  ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
+  vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
+  instance_type          = "t3.micro"
+
+  # 20GB is not enough
+  root_block_device {
+    volume_size = 50  # Set root volume size to 50GB
+    volume_type = "gp3"  # Use gp3 for better performance (optional)
+  }
+  user_data = file("docker.sh")
+  tags = {
+    Name    = "docker"
+  }
+}
+
+resource "null_resource" "docker" {
+  # Changes to any instance of the instance requires re-provisioning
+  triggers = {
+    instance_id = aws_instance.this.id
+  }
+
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  connection {
+    host = aws_instance.this.private_ip
+    type = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+  }
+
+  provisioner "file" {
+    source      = "docker.sh"
+    destination = "/tmp/docker.sh"
+  }
+
+  provisioner "remote-exec" {
+    # Bootstrap script called with private_ip of each node in the cluster
+    inline = [
+      "chmod +x /tmp/docker.sh",
+      "sudo sh /tmp/docker.sh ${var.aws_access_key} ${var.aws_secret_key}"
+    ]
+  }
+}
+
+resource "aws_security_group" "allow_all_docker" {
+  name        = "allow_all_docker"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+output "docker_ip" {
+  value       = aws_instance.this.public_ip
+}
